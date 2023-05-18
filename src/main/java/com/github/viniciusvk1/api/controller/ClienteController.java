a6161df2b057fe040177d5aab54f5b50299813cc
Vinicius <viniciusalmeida.vk1@gmail.com>
2023-05-18 16:37:23 -0300

    bean validation

--- src/main/java/com/github/viniciusvk1/api/controller/ClienteController.java
+++ src/main/java/com/github/viniciusvk1/api/controller/ClienteController.java
@@ -2,6 +2,8 @@ package com.github.viniciusvk1.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -48,12 +50,12 @@ public class ClienteController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@RequestBody Cliente cliente) {
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
 
 	@PutMapping("/{clienteId}")
-	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+	public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Long clienteId, @RequestBody Cliente cliente) {
 		if (!clienteRepository.existsById(clienteId)) {
 			return ResponseEntity.notFound().build();
 		}
--- src/main/java/com/github/viniciusvk1/domain/model/Cliente.java
+++ src/main/java/com/github/viniciusvk1/domain/model/Cliente.java
@@ -4,6 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -19,8 +23,19 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank
+	@Size(max = 60 , message = "O numero maximo de caracteres permitidos e de 60!")
+	@Size(min = 1, message = "O numero minimo de caracteres permitidos e de 1!")
 	private String nome;
+	
+	@NotBlank
+	@Email
+	@Size(max = 255)
 	private String email;
+	
+	@NotBlank
+	@Size(max = 20, message = "O numero maximo de caracteres permitidos para 'telefone' e de 20!")
 	private String telefone;
 	
 }
