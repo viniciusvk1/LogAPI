b9ae0df9603fdd3fc3ce97f269e47889b2f161bd
Vinicius <viniciusalmeida.vk1@gmail.com>
2023-05-17 23:59:12 -0300

    rework 2 - preparing for CRUD methods

--- src/main/java/com/github/viniciusvk1/api/controller/ClienteController.java
+++ src/main/java/com/github/viniciusvk1/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.github.viniciusvk1.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +13,12 @@ import com.github.viniciusvk1.domain.model.Cliente;
 @RestController
 public class ClienteController {
 
+	@PersistenceContext
+	private EntityManager manager;
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Vinicius Almeida");
-		cliente1.setEmail("vinicius@email.com");
-		cliente1.setTelefone("11 99576-2272");
-
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Camile Righetti");
-		cliente2.setEmail("camile@email.com");
-		cliente2.setTelefone("11 99532-6422");
-
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
-
 }
--- src/main/java/com/github/viniciusvk1/domain/model/Cliente.java
+++ src/main/java/com/github/viniciusvk1/domain/model/Cliente.java
@@ -1,45 +1,26 @@
 package com.github.viniciusvk1.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+@Entity
 public class Cliente {
-
+	
+	@EqualsAndHashCode .Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
 	private String nome;
-
 	private String email;
-
 	private String telefone;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-
+	
 }
